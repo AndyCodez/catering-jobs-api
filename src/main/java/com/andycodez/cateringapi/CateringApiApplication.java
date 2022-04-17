7117b6c8860ed3a6a7b88abc18b2f0da5c34b611
Andrew Owino <andystudy4@gmail.com>
2022-04-17 17:44:47 +0300

    Initial commit

---
+++ src/main/java/com/andycodez/cateringapi/CateringApiApplication.java
@@ -0,0 +1,13 @@
+package com.andycodez.cateringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CateringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CateringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andycodez/cateringapi/CateringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andycodez.cateringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CateringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
